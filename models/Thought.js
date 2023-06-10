de18a33c6b3ba8e0841b8275991b7c4ea54b3a16
SaintOfMirren <drew.miller2015@gmail.com>
2023-06-10 10:08:50 -0700

    updated models, added the rest of the routes

--- models/Thought.js
+++ models/Thought.js
@@ -1,51 +1,65 @@
 const { Schema, model } = require('mongoose');
 
-const thoughtSchema = new Schema(
+const reactionSchema = new Schema(
   {
-    thoughtText: {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
       type: String,
       required: true,
-      minLength: 1,
-      maxLength: 280,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAt) => new Intl.DateTimeFormat('en-US').format(createdAt),
+      maxlength: 280,
     },
     username: {
       type: String,
       required: true,
     },
-    reactions: [
-      {
-        reactionId: {
-          type: Schema.Types.ObjectId,
-          default: () => new Types.ObjectId(),
-        },
-        reactionBody: {
-          type: String,
-          required: true,
-          maxLength: 280,
-        },
-        username: {
-          type: String,
-          required: true,
-        },
-        createdAt: {
-          type: Date,
-          default: Date.now,
-          get: (createdAt) => new Intl.DateTimeFormat('en-US').format(createdAt),
-        },
-      },
-    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
   },
   {
     toJSON: {
       getters: true,
     },
+    id: false,
   }
 );
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
 
 const Thought = model('Thought', thoughtSchema);
 
--- models/User.js
+++ models/User.js
@@ -12,7 +12,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      match: [/.+@.+\..+/, 'Please enter a valid email address'],
+      match: [/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/, 'Please enter a valid email address'],
     },
     thoughts: [
       {
